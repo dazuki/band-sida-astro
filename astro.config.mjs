584237bbd8b52dad626cfa32d65ef2e054a09c1d
Lars Lindberg <absolute.dessan@gmail.com>
2024-02-17 19:40:07 +0100

    vite settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     integrations: [tailwind()],
     prefetch: true,
+    vite: {
+        build: {
+            emptyOutDir: false,
+        },
+    },
 });
