c6ac211545f296e0213e43e2e6973318d1599afc
Lars Lindberg <absolute.dessan@gmail.com>
2024-02-18 11:22:40 +0100

    assets folder name change: "_astro" -> "assets"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     integrations: [tailwind()],
     prefetch: true,
-    vite: {
-        build: {
-            emptyOutDir: false,
-        },
+    build: {
+        assets: "assets",
     },
 });
